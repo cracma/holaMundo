cdcbd0197e751e2a92d7fcd4227469d463e109e6
Marco <cracma@gmail.com>
2024-11-17 14:35:06 -0500

    Descripción breve de los cambios realizados

--- src/main/java/Ejemplo/Arreglo_1.java
+++ src/main/java/Ejemplo/Arreglo_1.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 
 public class Arreglo_1 {
+    
 
     public static void main(String[] args) {
+        System.out.println("cambios");
         
         //Defino el el arreglo
         int[] numeros = new int[5];
